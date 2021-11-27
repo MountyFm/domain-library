20c99065c4296428acc5d2371448a6f6dfe1c3e9
DSansyzbayev <sansyzbayevdaniyar3@gmail.com>
2021-11-27 21:56:10 +0600

    added amqp library

--- src/main/scala/kz/mounty/fm/amqp/messages/Messages.scala
+++ src/main/scala/kz/mounty/fm/amqp/messages/Messages.scala
@@ -4,9 +4,30 @@ package kz.mounty.fm.amqp.messages
 object MountyMessages {
   object UserProfileCore {
     case object Ping extends BaseMessage
+
+    case object CreateUserProfileRequest extends BaseMessage
+
+    case object UpdateUserProfileRequest extends BaseMessage
+
+    case object DeleteUserProfileRequest extends BaseMessage
+
+    case object GetUserProfileByIdRequest extends BaseMessage
+
+    case object GetUserProfileRooms extends BaseMessage
   }
   object MountyApi {
     case object Pong extends BaseMessage
+
+    case object CreateUserProfileResponse extends BaseMessage
+
+    case object UpdateUserProfileResponse extends BaseMessage
+
+    case object DeleteUserProfileResponse extends BaseMessage
+
+    case object GetUserProfileByIdResponse extends BaseMessage
+
+    case object GetUserProfileRoomsResponse extends BaseMessage
+
   }
 }
 
