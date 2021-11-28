fec42add13a472d662ca9beb1b6358505ea87e9a
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-28 10:10:45 +0600

    some changes

--- src/main/scala/kz/mounty/fm/domain/commands/ChangePlayerState.scala
+++ src/main/scala/kz/mounty/fm/domain/commands/ChangePlayerState.scala
@@ -3,6 +3,6 @@ package kz.mounty.fm.domain.commands
 import kz.mounty.fm.domain.DomainEntity
 import kz.mounty.fm.domain.player.PlayerState
 
-class ChangePlayerStateRequestBody(state: PlayerState) extends DomainEntity
+case class ChangePlayerStateRequestBody(state: PlayerState) extends DomainEntity
 
-class ChangePlayerStateResponseBody() extends DomainEntity
+case class ChangePlayerStateResponseBody() extends DomainEntity
--- src/main/scala/kz/mounty/fm/domain/requests/UpdateUserProfileRequest.scala
+++ src/main/scala/kz/mounty/fm/domain/requests/UpdateUserProfileRequest.scala
@@ -1,7 +1,6 @@
 package kz.mounty.fm.domain.requests
 
 import kz.mounty.fm.domain.DomainEntity
-import org.joda.time.DateTime
 
 case class UpdateUserProfileRequestBody(id: String,
                                     name: Option[String],
