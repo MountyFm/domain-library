fbacf6dd92c3e10b48d0423753e07f1727337c89
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-28 11:35:13 +0600

    changed lib version

--- src/main/scala/kz/mounty/fm/domain/commands/PlayerPlayGatewayCommand.scala
+++ src/main/scala/kz/mounty/fm/domain/commands/PlayerPlayGatewayCommand.scala
@@ -2,6 +2,6 @@ package kz.mounty.fm.domain.commands
 
 import kz.mounty.fm.domain.DomainEntity
 
-case class PlayerPlayGatewayCommandBody(deviceId: Option[String], tokenKey: String) extends DomainEntity
+case class PlayerPlayGatewayCommandBody(deviceId: Option[String], offset: Int = 0, tokenKey: String) extends DomainEntity
 
 case class PlayerPlayGatewayResponseBody() extends DomainEntity
