c7b284e1c6eb8884127949227f9f69c172e1fa25
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-28 11:38:56 +0600

    added contextUri

--- src/main/scala/kz/mounty/fm/domain/commands/PlayerPlayGatewayCommand.scala
+++ src/main/scala/kz/mounty/fm/domain/commands/PlayerPlayGatewayCommand.scala
@@ -2,6 +2,9 @@ package kz.mounty.fm.domain.commands
 
 import kz.mounty.fm.domain.DomainEntity
 
-case class PlayerPlayGatewayCommandBody(deviceId: Option[String], offset: Int = 0, tokenKey: String) extends DomainEntity
+case class PlayerPlayGatewayCommandBody(deviceId: Option[String],
+                                        contextUri: String,
+                                        offset: Int = 0,
+                                        tokenKey: String) extends DomainEntity
 
 case class PlayerPlayGatewayResponseBody() extends DomainEntity
