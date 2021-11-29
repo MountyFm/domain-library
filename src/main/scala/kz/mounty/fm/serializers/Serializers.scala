ff48947bdf02625fd395e9f96d5d79fa7aa58795
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-29 16:57:23 +0600

    added RoomType to serializers

--- src/main/scala/kz/mounty/fm/serializers/Serializers.scala
+++ src/main/scala/kz/mounty/fm/serializers/Serializers.scala
@@ -4,6 +4,7 @@ import kz.mounty.fm.domain.DomainEntity
 import kz.mounty.fm.domain.commands._
 import kz.mounty.fm.domain.playlist.Playlist
 import kz.mounty.fm.domain.requests._
+import kz.mounty.fm.domain.room.{Room, RoomStatus}
 import kz.mounty.fm.domain.track.Track
 import kz.mounty.fm.exceptions.ExceptionInfo
 import org.joda.time.DateTime
@@ -12,6 +13,21 @@ import org.json4s.jackson.Serialization
 import org.json4s.{CustomSerializer, Formats, JString, ShortTypeHints}
 
 trait Serializers {
+
+  class DateTimeSerializer extends CustomSerializer[DateTime](_ => ( {
+    case JString(s) => ISODateTimeFormat.dateTime().parseDateTime(s)
+  }, {
+    case d: DateTime => JString(ISODateTimeFormat.dateTime().print(d))
+  }
+  ))
+
+  class RoomStatusSerializer extends CustomSerializer[RoomStatus](_ => ( {
+    case JString(s) => RoomStatus(s)
+  }, {
+    case s: RoomStatus => JString(s.toString())
+  }
+  ))
+
   implicit val formats: Formats = Serialization.formats(
     ShortTypeHints(
       List(
@@ -40,16 +56,8 @@ trait Serializers {
         classOf[GetRoomByInviteCodeResponseBody],
         classOf[UpdateRoomRequestBody],
         classOf[UpdateRoomResponseBody],
+        classOf[Room],
       )
     )
-  ) + new DateTimeSerializer
-
-
-  class DateTimeSerializer extends CustomSerializer[DateTime](_ => ( {
-    case JString(s) => ISODateTimeFormat.dateTime().parseDateTime(s)
-  }, {
-    case d: DateTime => JString(ISODateTimeFormat.dateTime().print(d))
-  }
-  ))
-
+  ) + new DateTimeSerializer + new RoomStatusSerializer()
 }
